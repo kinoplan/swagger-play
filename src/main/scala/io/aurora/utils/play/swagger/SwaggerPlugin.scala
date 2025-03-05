acafb67ad664765f38a942899ed52813043a476f
Сариев Эдуард <sariev@kinoplan.ru>
2025-03-05 17:32:24 +0300

    Update Play to 3.0.6 and bump other libs

--- src/main/scala/io/aurora/utils/play/swagger/SwaggerPlugin.scala
+++ src/main/scala/io/aurora/utils/play/swagger/SwaggerPlugin.scala
@@ -1,13 +1,19 @@
 package io.aurora.utils.play.swagger
 
 import java.io.File
+
 import javax.inject.Inject
 
 import scala.io.Source
 
 import io.swagger.v3.oas.models.OpenAPI
 import play.api.{Configuration, Environment}
-import play.routes.compiler.{RoutesFileParser, StaticPart, Include => PlayInclude, Route => PlayRoute}
+import play.routes.compiler.{
+  Include => PlayInclude,
+  Route => PlayRoute,
+  RoutesFileParser,
+  StaticPart
+}
 
 trait SwaggerPlugin {
   def config: PlaySwaggerConfig
@@ -18,23 +24,22 @@ trait SwaggerPlugin {
   def getOpenApi: OpenAPI = apiListingCache.listing(config.host)
 }
 
-class SwaggerPluginImpl @Inject()(environment: Environment, configuration: Configuration) extends SwaggerPlugin {
+class SwaggerPluginImpl @Inject() (environment: Environment, configuration: Configuration)
+    extends SwaggerPlugin {
   lazy val config: PlaySwaggerConfig = PlaySwaggerConfig.getConfig
 
   lazy val routes = new RouteWrapper({
     val routesFile = configuration.get[Option[String]]("play.http.router") match {
-      case None => "routes"
+      case None        => "routes"
       case Some(value) => SwaggerPluginHelper.playRoutesClassNameToFileName(value)
     }
 
-    SwaggerPluginHelper
-      .parseRoutes(routesFile, "", environment)
-      .map { route =>
-        val call = route.call
-        val routeName = (call.packageName.toSeq ++ Seq(call.controller + "$", call.method)).mkString(".")
-        routeName -> route
-      }
-      .toMap
+    SwaggerPluginHelper.parseRoutes(routesFile, "", environment).map { route =>
+      val call = route.call
+      val routeName = (call.packageName.toSeq ++ Seq(call.controller + "$", call.method))
+        .mkString(".")
+      routeName -> route
+    }.toMap
   })
 
   lazy val scanner = new PlayApiScanner(routes, environment)
@@ -42,43 +47,39 @@ class SwaggerPluginImpl @Inject()(environment: Environment, configuration: Confi
 }
 
 object SwaggerPluginHelper extends Loggable {
-  def playRoutesClassNameToFileName(className: String): String = className.replace(".Routes", ".routes")
+
+  def playRoutesClassNameToFileName(className: String): String = className
+    .replace(".Routes", ".routes")
 
   def parseRoutes(routesFile: String, prefix: String, env: Environment): List[PlayRoute] = {
     logger.debug(s"Processing route file '$routesFile' with prefix '$prefix'")
 
-    val parsedRoutes = env
-      .resourceAsStream(routesFile)
-      .map { stream =>
-        val routesContent = Source.fromInputStream(stream).mkString
-        RoutesFileParser.parseContent(routesContent, new File(routesFile))
-      }
-      .getOrElse(Right(List.empty))
+    val parsedRoutes = env.resourceAsStream(routesFile).map { stream =>
+      val routesContent = Source.fromInputStream(stream).mkString
+      RoutesFileParser.parseContent(routesContent, new File(routesFile))
+    }.getOrElse(Right(List.empty))
 
     val routes = parsedRoutes
-      .getOrElse(throw new NoSuchElementException("Parsed routes not found!"))
-      .collect {
+      .getOrElse(throw new NoSuchElementException("Parsed routes not found!")).collect {
         case route: PlayRoute =>
           logger.debug(s"Adding route '$route'")
 
           (prefix, route.path.parts) match {
             case ("", _) => Seq(route)
-            case (_, Seq()) => Seq(route.copy(path = route.path.copy(parts = StaticPart(prefix) +: route.path.parts)))
+            case (_, Seq()) =>
+              Seq(route.copy(path = route.path.copy(parts = StaticPart(prefix) +: route.path.parts)))
             case (_, Seq(StaticPart(""))) =>
               Seq(route.copy(path = route.path.copy(parts = StaticPart(prefix) +: route.path.parts)))
             case (_, Seq(StaticPart("/"))) =>
               Seq(route.copy(path = route.path.copy(parts = StaticPart(prefix) +: route.path.parts)))
-            case (_, _) =>
-              Seq(route.copy(path = route.path.copy(parts = StaticPart(prefix) +: StaticPart("/") +: route.path.parts)))
+            case (_, _) => Seq(route.copy(path =
+                route.path.copy(parts = StaticPart(prefix) +: StaticPart("/") +: route.path.parts)
+              ))
           }
         case include: PlayInclude =>
           logger.debug(s"Processing route include $include")
 
-          val newPrefix = if (prefix == "") {
-            include.prefix
-          } else {
-            s"$prefix/${include.prefix}"
-          }
+          val newPrefix = if (prefix == "") include.prefix else s"$prefix/${include.prefix}"
 
           parseRoutes(playRoutesClassNameToFileName(include.router), newPrefix, env)
       }.flatten
@@ -87,4 +88,5 @@ object SwaggerPluginHelper extends Loggable {
 
     routes
   }
+
 }
--- src/main/scala/io/aurora/utils/play/swagger/modules/SwaggerModule.scala
+++ src/main/scala/io/aurora/utils/play/swagger/modules/SwaggerModule.scala
@@ -2,12 +2,12 @@ package io.aurora.utils.play.swagger.modules
 
 import io.aurora.utils.play.swagger.{SwaggerPlugin, SwaggerPluginImpl}
 import io.aurora.utils.play.swagger.controllers.SwaggerController
-import play.api.inject.{Binding, Module}
 import play.api.{Configuration, Environment}
+import play.api.inject.{Binding, Module}
 
 class SwaggerModule extends Module {
-  override def bindings(environment: Environment, configuration: Configuration): Seq[Binding[_]] =  Seq(
-    bind[SwaggerPlugin].to[SwaggerPluginImpl].eagerly(),
-    bind[SwaggerController].toSelf.eagerly()
-  )
+
+  override def bindings(environment: Environment, configuration: Configuration): Seq[Binding[_]] =
+    Seq(bind[SwaggerPlugin].to[SwaggerPluginImpl].eagerly(), bind[SwaggerController].toSelf.eagerly())
+
 }
